f98a88446fd9db2cbdd1d0be61deeef6ae2ca626
Paul Huckell <paul@huckleberry.studio>
2022-08-15 12:50:01 +0100

    Add front-page water hero

--- front-page.php
+++ front-page.php
@@ -1,9 +1,12 @@
 <?php get_header(); ?>
 
-<section class="w-full min-h-screen bg-gray-200 dark:bg-black justify-center flex">
-    <div class="pt-8 text-center">
-        <h1 class="text-black dark:text-white">Title</h1>
-        <span class="text-black dark:text-white">paragraph of supporting text.</span>
+<section class="relative h-screen flex flex-col items-center justify-center text-center text-black py-0 px-3 bg-white dark:bg-black">
+    <div class="div-video absolute top-0 left-0 w-full h-full overflow-hidden">
+        <video class="min-w-full min-h-full absolute object-cover" src="/wp-content/uploads/2022/08/pexels-rostislav-uzunov-7385122.mp4" type="video/mp4" autoplay muted loop></video>
+    </div>
+    <div class="z-10">
+        <h1 class="">Huckleberry Studio</h1>
+        <p class="">WordPress development with search engine optimisation as standard.</p>
     </div>
 </section>
 
